3cd0f5d28dfb5f31fb73cd60a6fd550a5cde89b3
Paulo Merson <Paulo Merson>
2017-05-04 02:03:21 -0300

    last minute tuning

--- apps/consumerwebsite/src/java/com/sun/j2ee/blueprints/consumerwebsite/actions/CatalogHTMLAction.java
+++ apps/consumerwebsite/src/java/com/sun/j2ee/blueprints/consumerwebsite/actions/CatalogHTMLAction.java
@@ -29,7 +29,7 @@ import com.sun.j2ee.blueprints.waf.controller.web.html.HTMLActionException;
  * to access the database.
  */
 public final class TO_BE_RENAMED implements HTMLAction {
-// public final class CatalogHTMLAction implements HTMLAction {
+//public final class CatalogHTMLAction implements HTMLAction {
 
     public static final String ACCOUNT_READ_ACTION = "readAccount";
     public static final String ACCOUNT_CREATE_ACTION = "createAccount";
